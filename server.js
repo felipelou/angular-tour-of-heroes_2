4958e20d36eaf9130a0cd3d0291697c895566c97
Felipe Alejandro Rosas Mendoza <felipelou123499@gmail.com>
2022-11-10 20:46:43 -0600

    si

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-tour-of-heroes'));
+app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-tour-of-heroes/'}),
+    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
 );
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
